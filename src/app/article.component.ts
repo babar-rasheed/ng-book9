0e2c2b9fe0e72246f7a2bb88d75bf6f04bbbe9d7
babar-rasheed <babar.rasheed.isb@gmail.com>
2021-01-19 07:27:51 +0300

    inventory app start

--- src/app/article.component.ts
+++ src/app/article.component.ts
@@ -1,28 +1,8 @@
 import { Component, Input, HostBinding, OnInit } from "@angular/core";
-import { Article } from "./article.model";
 
 @Component({
   selector: "app-article",
   templateUrl: "./article.component.html",
   styles: []
 })
-export class ArticleComponent implements OnInit {
-  @HostBinding("attr.class") cssClass = "row";
-
-  @Input() article: Article;
-  constructor() {
-    // this.article = new Article("Angular", "http://angular.io", 10);
-  }
-
-  voteUp(): boolean {
-    this.article.voteUp();
-    return false;
-  }
-
-  voteDown(): boolean {
-    this.article.voteDown();
-    return false;
-  }
-
-  ngOnInit() {}
-}
+export class ArticleComponent {}
